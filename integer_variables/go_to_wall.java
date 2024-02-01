76183c1f653f2dcd3685e1315db2370288cd3bc3
dcre <107809443+realdcre@users.noreply.github.com>
2024-02-01 16:20:52 +0100

    changed go_to_wall.java

---
+++ integer_variables/go_to_wall.java
@@ -0,0 +1,36 @@
+int stepAmount = 0;
+    //A2//
+int order = 69;
+int pickupAmount = 0;
+public void goToWall() {
+    while (vornFrei()){
+        vor();
+        stepAmount = stepAmount + 1;
+    }
+    //A1//
+
+}
+public void pickup(){
+    
+    while(pickupAmount < order){
+        nimm();
+        pickupAmount = pickupAmount + 1;
+    }
+}
+public void dropoff(){
+    stepAmount = stepAmount * 2
+    pickupAmount = pickupAmount - stepAmount
+    while(pickupAmount > 0) {
+        gib();
+    }
+}
+public void main(){
+    goToWall();
+    pickup();
+    linksUm();
+    linksUm();
+    goToWall();
+    dropoff();
+}
+
+    
