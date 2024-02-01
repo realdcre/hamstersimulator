b925a49365d0d90d67e1c53d826c0311eac713a9
dcre <107809443+realdcre@users.noreply.github.com>
2024-02-01 15:18:00 +0100

    created hamster_to_wall1try2

---
+++ hamster_to_wall1try2/hamster_to_wall1.java
@@ -0,0 +1,15 @@
+boolean cornAmountEven = true
+
+public void main() {
+    while (vornFrei){
+        if (kornDa) {
+            nimm();
+            if (cornAmountEven == true){
+                cornAmountEven = false;
+            } else if (cornAmountEven == false){
+                cornAmountEven = true;
+            }
+        }
+        vor();
+    }
+}
