87b9f00bf74a1e80d5c5162b42fa4ad8daf55218
dcre <107809443+realdcre@users.noreply.github.com>
2024-02-01 16:03:46 +0100

    boolean_either_side_empty_java1

--- boolean_either_side_empty/boolean_either_side_empty1.java
+++ boolean_either_side_empty/boolean_either_side_empty1.java
@@ -1,3 +1,4 @@
-boolean pathToEitherSideEmpty(){
-    
-}
+true    true    false       !&&=false
+true    false   true        !&&=true ||=true
+true    false   true        !&&=true ||=true
+false   false   false       &&=true
