0d73565ea595928228775d73228d2fd72eed3c5e
jacobco@Ursulinen-Gymnasium <158718792+ugm-jacobco@users.noreply.github.com>
2024-02-08 16:29:22 +0100

    created java_mission.java

---
+++ hamster_mission/hamster_mission.java
@@ -0,0 +1,52 @@
+int stepAmount;
+
+public void move7(){
+    for (int i = 0; i < 7; i++) {
+    vor();
+}
+}
+
+
+public void multipleStep(int stepAmount){
+    for (int i = 0; i < stepAmount; i++){
+        vor();
+    }
+}
+
+public int cornAmount(){
+    int lcornAmount = 0;
+    while(kornDa()){
+        nimm();
+        lcornAmount++;
+    }
+    while(!maulLeer()){
+        gib();
+    }
+    return lcornAmount;
+}
+boolean cornAmount0(int cornAmount){
+	if (cornAmount == 0){
+		return true;
+	} else {
+		return false;
+	}
+}
+int walkToNextCorn(){
+    int cornAmount = cornAmount();
+    linksUm();
+    linksUm();
+    linksUm();
+    multipleStep(cornAmount);
+    return cornAmount;
+   
+}
+
+	
+void main(){
+	move7();
+	int cornAmount = walkToNextCorn();
+    while(cornAmount0(cornAmount())){
+    	walkToNextCorn();
+
+    }
+}
