4873310d5747f9174db1861f28fccd1ed1d39fe1
jacobco@Ursulinen-Gymnasium <158718792+ugm-jacobco@users.noreply.github.com>
2024-02-22 15:40:23 +0100

    added revision_variables.java and output1.java in the appropriate folders

---
+++ hamster console+/output1.java
@@ -0,0 +1,10 @@
+void print(int number) {
+	System.out.println(text);
+}
+void main(){
+
+int number = 69420
+String text = "This was send from Hamster Simulator!!!";
+print(text);
+}
+
---
+++ revision variables/revision_variables.java
@@ -0,0 +1,8 @@
+public void main(){
+    boolean studentIsIn = false
+    int grade = 1
+
+}
+
+    String name = "realdcre"
+    char = "C" 
